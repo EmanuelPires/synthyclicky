c0d58064b2fc39ba985c187e114a1b3f81777b8a
Emanuel Pires <emanuelpires11@gmail.com>
2019-03-31 13:25:05 -0400

    Updates

---
+++ precache-manifest.579bde44592c87fa42130726d4ab50ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "693c74116c0aa8d3c5ff",
+    "url": "/synthyclicky/static/js/runtime~main.7908a281.js"
+  },
+  {
+    "revision": "f4674f53b860ee997317",
+    "url": "/synthyclicky/static/js/main.650ba7e1.chunk.js"
+  },
+  {
+    "revision": "0a3b2f7205b9881cb369",
+    "url": "/synthyclicky/static/js/2.3c76d457.chunk.js"
+  },
+  {
+    "revision": "f4674f53b860ee997317",
+    "url": "/synthyclicky/static/css/main.04b3e518.chunk.css"
+  },
+  {
+    "revision": "7117ce0ab93d6ca1c15a065e0409d67a",
+    "url": "/synthyclicky/index.html"
+  }
+];
